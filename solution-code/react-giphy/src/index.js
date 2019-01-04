b067acf49335ca84c91960654ce33cf052364650
ishaarora <isha.arora@generalassemb.ly>
2019-01-04 15:07:01 -0800

    created a solution branch for solution code

--- solution-code/react-giphy/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './Home';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();

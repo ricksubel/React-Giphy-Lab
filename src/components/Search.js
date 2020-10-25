c8de3189557f27170c0424dc357fb52dd58154a7
ricksubel <ricksubel@gmail.com>
2020-10-25 16:34:26 -0400

    Wired up Search Button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
-const Search = (props) => {
-    return(
-        <div>
-            <input 
-                type="text" 
-                placeholder="Search here"
-                onKeyUp={props.onInput}
-            />
-        </div>
-    )
+class Search extends React.Component {
+    state = { 
+        query: '',
+    }
+
+    search = () => {
+        console.log(this.state.query)
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.search}>
+                <input
+                type='text'
+                name='query'
+                onChange={this.handleChange}
+                value={this.state.query} />
+                <input type='submit' value='Submit'/>
+            </form>
+            </div>
+        )
+    }
 }
+
 
 export default Search;
